35a0d189052e665e79118e48722e687d9d374b42
Lucas Bernardes <lucascbernardes@live.com>
2017-08-16 16:58:39 -0300

    Geração da solução inicial consertada

--- src/core.clj
+++ src/core.clj
@@ -4,66 +4,76 @@
   (:gen-class))
 
 ;; Problem specs
+;; x -> route matrix
+;; d -> distance matrix
+;; q -> max capacity of a route
 (def vrp (read-string (slurp "A-n32-k5.txt")))
 (def customers (range 2 (:dimension vrp)))
 
+;; Test cases
+(def d (dge 3 3 [0 3 3 3 0 2 3 2 0] {:order :row}))
+(def r (dge 3 3 [0 1 0 0 0 1 1 0 0] {:order :row}))
+
 ;; Utils
-(defn int->key
-  "Converts an integer to a hashmap keyword"
-  [v]
-  (keyword (str v)))
+(defn route-cost
+  "Returns the cost of the route x"
+  [x d]
+  (reduce + (for [i (range (mrows x))]
+              (dot (row x i)
+                   (row d i)))))
 
-(defn demand
-  "Gets the demand of a customer"
+(defn choose-customer
+  "Chooses randomly a customer and return it"
   [cust]
-  ((int->key cust) (:demands vrp)))
+  (if (finished? cust)
+    -1
+    (let [chosen-cust (rand-nth (range 1 (dim cust)))]
+      (if (= (entry cust chosen-cust) 0.0)
+        chosen-cust
+        (recur cust)))))
 
-(defn in?
-  "Checks if an element belongs to a collection"
-  [elem coll]
-  (some #(= elem %) coll))
-
-(defn at
-  "Equivalent to m[i][j]"
-  [m i j]
-  (nth (nth m i) j))
-
-(defn cost
-  "Returns the cost of a tour"
-  [dist-matrix tour]
-  (reduce + (apply dot )))
-
-(defn calculate-load
-  "Calculates the total load of a tour"
-  [tour]
-  (reduce + (map demand tour)))
+(defn finished?
+  "True if there is any customer available, or false otherwise"
+  [cust]
+  (= (int (sum cust)) (dec (dim cust))))
 
 ;; Generates the initial solution
-(defn generate-tour
-  [customers dists last tour cap]
-  (let [chosen-cust (rand-nth customers)]
-    (if (> (+ (cost tour)
-              (entry dists last chosen-cust)
-              (entry dists chosen-cust 0))
-            cap)
-      (entry! tour last 0 1)
-      (recur (remove #(= % chosen-cust) customers)
-             dists
-             chosen-cust
-             (entry! tour last chosen-cust 1)
-             cap))))
+(defn generate-route
+  "Generates a route x randomly from a list of available customers with distances d for a vehicle with capacity q"
+  [cust last x d q]
+  (if (finished? cust)
+    (entry! x last 0 1)
+    (let [chosen-cust (choose-customer cust)]
+      (println last chosen-cust)
+      (if (> (+ (route-cost x d)
+                (entry d last chosen-cust)
+                (entry d chosen-cust 0))
+             q)
+        (entry! x last 0 1)
+        (recur (entry! cust chosen-cust 1)
+               chosen-cust
+               (entry! x last chosen-cust 1)
+               d
+               q)))))
 
 (defn generate-initial-solution
-  "Generates randomly a initial feasible solution with k vehicles"
-  [n k d c]
-  (for [i (range k)]
-    (generate-tour (range n) d 0 (dge n n) c)))
+  "Generates randomly a initial feasible solution with k routes"
+  [k d q]
+  (let [n (ncols d)
+        cust (dv n)]
+    (for [i (range k)]
+      (generate-route cust 0 (dge n n) d q))))
 
-;; Heuristica de remocao
+;; Removal heuristic
+(defn cost-without-customer
+  "Removes the customer i from the route and recalculates the cost"
+  [route i]
+  ("oi"))
+
 (defn worst-removal
   "Removes randomly the worst costumer in the tour"
   []
-  ())
+  ("oi"))
 
 (defn -main
   "I don't do a whole lot ... yet."
